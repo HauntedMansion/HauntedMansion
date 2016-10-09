d1fed0569a4407c86b730b2979bb15be293dfa9a
Eric Oudin <Eric_Oudin29@yahoo.com>
2016-10-09 16:40:33 -0400

    Mansion random rooms attach. Everything is pretty much blank so far though. Just needs to be filled in with assets.

---
+++ Haunted Manor/Assets/Scripts/RandomRoomGenerator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class RandomRoomGenerator : MonoBehaviour {
+
+  public GameObject emptyRoom;
+
+  GameObject[] doorways;
+
+	void Start ()
+  {
+    doorways = GameObject.FindGameObjectsWithTag("Doorway");
+    AddRoomsOnDoorways();
+	}
+  void AddRoomsOnDoorways()
+  {
+    for(int i = 0; i < doorways.Length; i++)
+    {
+      Instantiate(emptyRoom, doorways[i].transform.localPosition, doorways[i].transform.localRotation);
+    }
+  }
+
+}
