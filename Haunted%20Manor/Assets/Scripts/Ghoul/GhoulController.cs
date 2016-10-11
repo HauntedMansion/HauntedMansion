2c9d30fbb12e31757d4b4e663016b3708d69ed9e
HauntedMansion <rsierr01@baker.edu>
2016-10-10 20:53:12 -0400

    Add files via upload

---
+++ Haunted%20Manor/Assets/Scripts/Ghoul/GhoulController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhoulController : MonoBehaviour {
+	int health;
+	public GameObject player;
+	public int slowDist;
+	public float moveSpeed;
+	public float slowSpeed;
+	public float teleportAfterShot = 7;
+
+	// Use this for initialization
+	void Start () {
+		health = 200;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (health <= 0)
+			Destroy (this.gameObject);
+
+		transform.LookAt (player.transform);
+		if (Vector3.Distance (transform.position, player.transform.position) <= slowDist) {
+			transform.position += transform.forward * slowSpeed * Time.deltaTime;
+		} else {
+			transform.position += transform.forward * moveSpeed * Time.deltaTime;
+		}
+	}
+
+	public void takeDamage (int damage)
+	{
+		health -= damage;
+	}
+
+	public int returnHealth()
+	{
+		return health;
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player")
+			Destroy (other.gameObject);
+
+		if (other.tag == "Bullet") {
+			transform.position -= transform.forward * teleportAfterShot;
+		}
+	}
+}
